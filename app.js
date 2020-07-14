8503ba5029813b06e154424f5f559f071c35b992
kimjuho <soulkimlively@gmail.com>
2020-07-14 14:45:03 +0900

    추가

---
+++ app.js
@@ -0,0 +1,96 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: '안녕하세요 Vue!'
+    }
+})
+
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+        message: '이 페이지는 ' + new Date() + ' 에 로드 되었습니다'
+    }
+})
+
+var app3 = new Vue({
+    el: '#app-3',
+    data: {
+        seen: true
+    }
+})
+
+var app4 = new Vue({
+    el: '#app-4',
+    data: {
+        todos: [
+            { text: 'JavaScript 배우기' },
+            { text: 'Vue 배우기' },
+            { text: '무언가 멋진 것을 만들기' }
+        ]
+    }
+})
+
+var app5 = new Vue({
+    el: '#app-5',
+    data: {
+        message: '안녕하세요! Vue.js!'
+    },
+    methods: {
+        reverseMessage: function () {
+            this.message = this.message.split('').reverse().join('')
+        }
+    }
+})
+
+var app6 = new Vue({
+    el: '#app-6',
+    data: {
+        message: '안녕하세요 Vue!'
+    }
+})
+
+Vue.component('todo-item', {
+    props: ['todo'],
+    template: '<li>{{ todo.text }}</li>'
+})
+
+var app7 = new Vue({
+    el: '#app-7',
+    data: {
+        groceryList: [
+            { id: 0, text: 'Vegetables' },
+            { id: 1, text: 'Cheese' },
+            { id: 2, text: 'Whatever else humans are supposed to eat' }
+        ]
+    }
+})
+
+var app8 = new Vue({
+    el: '#app-8',
+    data: {
+        firstName: 'Foo',
+        lastName: 'Bar',
+        fullName: 'Foo Bar'
+    },
+    watch: {
+        firstName: function (val) {
+            this.fullName = val + ' ' + this.lastName
+        },
+        lastName: function (val) {
+            this.fullName = this.firstName + ' ' + val
+        }
+    }
+})
+
+var app9 = new Vue({
+    el: '#app-9',
+    data: {
+        firstName: 'Foo',
+        lastName: 'Bar'
+    },
+    computed: {
+        fullName: function () {
+            return this.firstName + ' ' + this.lastName
+        }
+    }
+})
